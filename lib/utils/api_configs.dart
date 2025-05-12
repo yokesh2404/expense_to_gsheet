09829347a12cc6339896c626d4cfd943645b47d3
Yokeshwaran Nagarajan <89254072+yokesh2404@users.noreply.github.com>
2025-05-12 14:01:05 +0530

    Update api_configs.dart

--- lib/utils/api_configs.dart
+++ lib/utils/api_configs.dart
@@ -1,4 +1,4 @@
 class ApiConfigs {
 
-  static const String scriptUrl='https://script.google.com/macros/s/AKfycbxXxtQogwLpfmcG_rS8Ht6Tsnj3oZx09Tpp2qUzqeAD1ck7mKaXWzP6Lhd5Bbdlk8Y/exec';
+  static const String scriptUrl='https://script.google.com/macros/s/your_deployment_id/exec';
 }
